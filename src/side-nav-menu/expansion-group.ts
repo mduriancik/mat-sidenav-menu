7849ef25ca03bc588d7881e20b1c2c2d4834f19f
mduriancik <mduriancik>
2017-10-24 07:24:58 +0200

    feat(expansion-group): add expansion-group

---
+++ src/side-nav-menu/expansion-group.ts
@@ -0,0 +1,23 @@
+import { AfterContentInit, Component, ContentChildren, EventEmitter, Input, Output, QueryList } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd, RouterStateSnapshot } from '@angular/router';
+import { MatSidenav } from '@angular/material';
+
+@Component({
+  selector: 'side-nav-x-group',
+  template:
+    `<mat-expansion-panel [hideToggle]="hideToggle">
+        <mat-expansion-panel-header *ngIf="title">{{title}}</mat-expansion-panel-header>
+        <ng-content></ng-content>
+    </mat-expansion-panel>`,
+  styles: [`
+    div.mat-expansion-panel-body {
+      padding-right: 0px;
+      padding-left: 0px;
+    }
+  `]
+})
+export class SideNavExpansionGroupComponent {
+  @Input('title') title: string;
+  @Input('hideToggle') hideToggle: string;
+
+}
--- src/side-nav-menu/module.ts
+++ src/side-nav-menu/module.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatRippleModule, MatToolbarModule } from '@angular/material';
+import { MatButtonModule, MatExpansionModule, MatIconModule, MatRippleModule, MatToolbarModule } from '@angular/material';
 
 import { SideNavMenuComponent } from './menu';
 import { SideNavItemComponent } from './item';
 import { SideNavGroupComponent } from './group';
+import { SideNavExpansionGroupComponent } from './expansion-group';
 
 
 @NgModule({
   declarations: [
-    SideNavGroupComponent, SideNavMenuComponent, SideNavItemComponent,
+    SideNavExpansionGroupComponent, SideNavGroupComponent, SideNavMenuComponent, SideNavItemComponent,
   ],
   imports: [
     BrowserModule, BrowserAnimationsModule,
-    MatButtonModule, MatIconModule, MatToolbarModule, MatRippleModule
+    MatButtonModule, MatExpansionModule, MatIconModule, MatToolbarModule, MatRippleModule
   ],
   exports: [
-    SideNavGroupComponent, SideNavItemComponent, SideNavMenuComponent
+    MatExpansionModule,
+    SideNavExpansionGroupComponent, SideNavGroupComponent, SideNavItemComponent, SideNavMenuComponent
   ]
 })
 export class SideNavMenuModule {}
