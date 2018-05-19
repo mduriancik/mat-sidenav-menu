dc6205f39eb5579a8524a5361f820e13d336294d
mduriancik <mduriancik>
2018-05-20 00:03:38 +0200

    fix(tests): karma tests configuration

--- projects/mat-sidenav-menu/src/lib/mat-sidenav-menu.component.spec.ts
+++ projects/mat-sidenav-menu/src/lib/mat-sidenav-menu.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MatSidenavMenuComponent } from './mat-sidenav-menu.component';
+import { MatIconModule, MatToolbarModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 describe('MatSidenavMenuComponent', () => {
   let component: MatSidenavMenuComponent;
@@ -8,7 +10,8 @@ describe('MatSidenavMenuComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MatSidenavMenuComponent ]
+      imports: [MatIconModule, MatToolbarModule, RouterModule.forRoot([], {useHash: true})],
+      declarations: [MatSidenavMenuComponent]
     })
     .compileComponents();
   }));
