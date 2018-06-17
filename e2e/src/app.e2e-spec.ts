0b23f28f35b6a5e7f45df050fd250284f28b7e84
mduriancik <mduriancik>
2018-06-17 08:03:51 +0200

    test(e2e): nav-item action, nav-item active

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { MatSidenavMenuPage } from './app.po';
-import { browser } from 'protractor';
+import { browser, by, element } from 'protractor';
 
 describe('mat-sidenav-menu App', () => {
   let page: MatSidenavMenuPage;
@@ -12,5 +12,21 @@ describe('mat-sidenav-menu App', () => {
     page.navigateTo();
     page.clickMenuButton();
     expect(page.getMenuTitle()).toEqual('Demo Menu');
+  });
+
+  it('nav-item action', () => {
+    page.navigateTo();
+    page.clickMenuButton();
+    element.all(by.css('side-nav-item a')).get(0).click();
+    expect(browser.getCurrentUrl()).toBe('http://localhost:4200/#/state');
+  });
+
+  it('nav-item active', () => {
+    page.navigateTo();
+    page.clickMenuButton();
+    element.all(by.css('side-nav-item a')).get(1).click();
+    // page.clickMenuButton(); // -> menu dont close if a click on an active item (bug? or not?)
+    const itemText = element(by.css('.side-nav-item-active span')).getText();
+    expect(itemText).toBe('Page 1');
   });
 });
