9e5c848f6e71e739b88eb3d44f96d6c675fd26b8
mduriancik <mduriancik>
2018-10-21 08:04:32 +0200

    feat(ng7): update to angular/cli/material 7

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -3,3 +3,6 @@ import fsx from 'fs-extra';
 
 fsx.removeSync('dist');
 console.log('cleaning ...');
+fsx.mkdirSync('dist');
+fsx.mkdirSync('dist/mat-sidenav-menu');
+console.log('./dist/mat-sidenav-menu created ...');
