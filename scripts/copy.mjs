4f30bbc898e40901cbb7847986ecd77505f7e0a8
mduriancik <mduriancik>
2018-10-28 17:58:12 +0100

    fix:  add README.md to npm package, add repo info to npm package

--- scripts/copy.mjs
+++ scripts/copy.mjs
@@ -1,5 +1,6 @@
 import fsx from 'fs-extra';
 
 fsx.copySync('./projects/mat-sidenav-menu/src/lib/mat-sidenav-menu-theme.scss', './dist/mat-sidenav-menu/mat-sidenav-menu-theme.scss');
+fsx.copySync('./README.md', './dist/mat-sidenav-menu/README.md');
 
 console.log('copying mat-sidenav-menu-theme.scss ...');
